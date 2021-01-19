f3813b319ea9ec3d32a60566937c71325de1453d
tjyanjing <jyan235@ucmerced.edu>
2021-01-19 09:01:35 -0800

    This is the first commit to rev01.

--- src/government/Justices.java
+++ src/government/Justices.java
@@ -1,4 +1,25 @@
 package government;
 
 public class Justices {
+    private static Justices instance;
+    static int i = 1;
+
+    private Justices(){
+    }
+
+    public void setJustices(int number_input){
+        i = number_input;
+    }
+
+    public static Justices getInstance(){
+        if (i <= 9){
+            System.out.println("The Justices no." + i + " :");
+            instance = new Justices();
+//            i++;
+        } else {
+            System.out.println("You've already had 9 justices!");
+            System.out.println("You cannot add more!");
+        }
+        return instance;
+    }
 }
